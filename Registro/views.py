473c0aec6b27660c03111902a48cafde29b1818e
Piruuux <angelo_piru@hotmail.com>
2023-03-09 17:13:52 -0300

    Superuser

--- Registro/views.py
+++ Registro/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from Registro.forms import *
+from django.shortcuts import render, redirect 
+from .forms import *
 from Registro.models import *
 from django.views.generic import *
+from django.contrib.auth import *
 
 
 # Create your views here.
@@ -13,3 +14,18 @@ class Usuario_Inscripsion(CreateView):
 
 def Completado(request):
     return render (request, 'Registro/completado.html')
+
+
+
+
+def guardar_formulario(request):
+    if request.method == 'POST':
+        form = FormRegistro(request.POST)
+        if form.is_valid():
+            # Guardamos los datos del formulario en la base de datos
+            Usuario = form.save()
+            Usuario.save()
+            return render(request, 'completado.html') # Redirigimos a una página de éxito
+    #else:
+        #form = FormRegistron()
+    #return render(request, 'Registro.html', {'form': form})s   
